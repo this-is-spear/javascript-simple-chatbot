beb0dd632af1c6a004cebf131c6ee1d1ad683859
imaspear <rjsckdd12@gmail.com>
2021-12-23 11:07:10 +0900

    12월 23일 스터디 공부
    전역 변수를 함수로 호출해 값을 올려주고 콘솔에 로그 출력하기

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-function changeText(i, string){
-    document.getElementsByClassName('typing')[i].innerHTML = "";
-    console.log(i)
-    var typingBool = false; 
-    var typingIdx=0; 
-    var typingTxt = string;
-    typingTxt=typingTxt.split("");
-    if(typingBool==false){ 
-        typingBool=true;     
-        var tyInt = setInterval(typing,100);
-    } 
-    function typing(){ 
-        if(typingIdx<typingTxt.length){ 
-            document.getElementsByClassName('typing')[i].append(typingTxt[typingIdx]);
-            typingIdx++; 
-        } else{ 
-            clearInterval(tyInt); 
-        } 
-    }
-    document.getElementsByClassName('typing')[i].setAttribute("disabled","disabled")
+let index = 0
+
+function consoleLog(){
+    // document.getElementsByClassName(".btn")
+    console.log(index++)
 }
