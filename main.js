6fb5e8cafbdc53455636d9272ca3df3f3cb6e41e
imaspear <rjsckdd12@gmail.com>
2021-12-24 17:39:44 +0900

    12월 24일 모각코 공부
    자바스크립트 변수 타입을 확인

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 let index = 0
 
 function consoleLog(){
-    // document.getElementsByClassName(".btn")
-    console.log(index++)
+    var i = 10;
+    console.log(typeof(i))
+    var i = "asdasd";
+    console.log(typeof(i))
+    var i = {name : "key", age : "adsf"};
+    console.log(typeof(i))
+    var i = null;
+    console.log(typeof(i))
+    var i = true;
+    console.log(typeof(i))
+    var i = undefined;
+    console.log(typeof(i))
 }
