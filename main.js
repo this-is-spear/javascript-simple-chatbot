b79a44976eb3ea35901c963f999752071534b5be
imaspear <rjsckdd12@gmail.com>
2021-12-28 15:10:15 +0900

    12월 28일 모각코 공부 자바스크립트를 이용해 스톱워치 만들기

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-let x = 5;
-let y = 6;
-console.log('x + y : ', x + y)
-console.log('x - y : ', x - y)
-console.log('x * y : ', x * y)
-console.log('x / y : ', x / y)
-console.log('x % y : ', x % y)
+let timerId;
+let time = 0;
+const stopwatch = document.getElementById("time");
+let  min, sec, mil;
+
+
+function printTime() {
+    time++;
+    stopwatch.innerText = getTimeFormatString();
+    clearTimeout(timerId);
+}
+
+function startClock() {
+    printTime();
+    timerId = setTimeout(startClock, 10);
+}
+
+function stopClock() {
+    if (timerId != null) {
+        clearTimeout(timerId);
+    }
+}
+
+function resetClock() {
+    stopClock()
+    stopwatch.innerText = "00:00:00";
+    time = 0;
+}
+
+function getTimeFormatString() {
+    min = Math.floor(time / (60 * 60));
+    sec = Math.floor((time - (min * 60 * 60)) / 100);
+    mil = time % 100;
+
+    return String(min).padStart(2, '0') + ":" + String(sec).padStart(2, '0') + ":" + String(mil).padStart(2, '0');
+}
