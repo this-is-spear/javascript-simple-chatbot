501fe6ad69d90fcd5ed6ae090c6b5d937007eeea
imaspear <rjsckdd12@gmail.com>
2022-01-04 16:17:43 +0900

    1월 4일 모각코

--- static/main.js
+++ static/main.js
@@ -1,26 +1,32 @@
 let says = document.querySelector(".cat_says")
-let message = ""
-console.log()
+let body = document.body
 function check_text() {
-    let value = document.getElementById("console").value;
-    console.log(value);
-    message = value
-    apply_chat();
+    // body.style.backgroundColor="white"
+    says.innerHTML = ""
+    let message = document.getElementById("console").value;
+    apply_chat(message);
 }
 
-function apply_chat() {
-    say_logic()
-    says.innerHTML = message;
+function apply_chat(message) {
+    if(message == "불꺼줘"){
+        document.body.style.backgroundColor="gray"
+        says.innerHTML = "냐야(OK)!"
+    }else if(message == ""){
+        says.innerHTML = "대답해"
+    }else{
+        says.innerHTML = say_logic(message);
+    }
 }
 
-function say_logic() {
+function say_logic(message) {
     if(message.length == 1){
-        message = "냐~!"
+        message = "냐~!";
     }else if (message.length % 2 == 1) {
         message = "냐" + "아".repeat(message.length - 2) 
                     + "~".repeat(Math.ceil((Math.random()*10)%10)) 
-                    + "앙~!"
+                    + "앙~!";
     }else{
-        message = "냐아 ".repeat(message.length) + "~" + "!".repeat(Math.ceil((Math.random()*10)%10)) 
+        message = "냐아 ".repeat(message.length) + "~" + "!".repeat(Math.ceil((Math.random()*10)%10));
     }
+    return message;
 }
